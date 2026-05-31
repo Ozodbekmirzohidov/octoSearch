4d2cc73fcfab93c392dfe08d11af62c07d510559
Ozodbek Mirzohidov <ozodbek.mirzohidov@icloud.com>
2026-05-31 21:08:47 +0500

    fix: sidebar items

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -42,25 +42,7 @@ export function Sidebar() {
             </svg>
           ),
         },
-        {
-          label: "Profile card",
-          href: session?.user?.name ? `/user/${session.user.name}` : "/user",
-          icon: (
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={1.5}
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-              />
-            </svg>
-          ),
-        },
+
         {
           label: "Search",
           href: "/search",
@@ -153,6 +135,25 @@ export function Sidebar() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
+              />
+            </svg>
+          ),
+        },
+        {
+          label: "Profile card",
+          href: session?.user?.name ? `/user/${session.user.name}` : "/user",
+          icon: (
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={1.5}
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
               />
             </svg>
           ),
